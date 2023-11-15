103aa684ed4aa1f236e993d10c46055ea4c9bdd8
Yurii <dets.yurii@gmail.com>
2023-11-15 15:15:22 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from 'components/App';
 import './index.css';
-import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
